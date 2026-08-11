16f25eafddac9e9bdf3603a51d14bc5818c8f05f
Diana <diana.avalos.m@gmail.com>
2026-08-11 14:54:27 +0200

    change init with new name

--- foundry/data/pipelines/__init__.py
+++ foundry/data/pipelines/__init__.py
@@ -1,5 +1,5 @@
 from .omni_ieeg import OmniIEEGPipeline
-from .nsb_epigrid import NsbEpigridV1Pipeline
+from .neurosoft_nsb_epigrid import NsbEpigridV1Pipeline
 
 __all__ = [
     "OmniIEEGPipeline",
